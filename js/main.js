cbd16f8211a1650d3bc938f77ebddca4248c4731
yuwei yang <yang0347@algonquinlive.com>
2023-06-02 13:03:46 -0400

    s

--- js/main.js
+++ js/main.js
@@ -1,7 +1,41 @@
 console.log("JS file added!")
 
-const region = "Canada"
+const region = "China"
 
-if(regin === "Canada"){
+if(regin = "Canada"){
     console.log("You are seeing Canadian version of this site")
 }
+
+let price = 20
+const shippingFee = 15
+let cartTotal
+
+if(price >= 50){
+    cartTotal = price
+} else{
+    cartTotal = price + shippingFee
+}
+
+//console.log(cartTotal)
+
+document.getElementById("cart1").innerHTML += "Your cart total is " + cartTotal
+
+//reassigning the price
+
+price = 105
+const discount = 0.15
+
+if(price >= 100){
+    cartTotal = price - (price * discount)
+} else if(price >= 50){
+    cartTotal = price
+} else{
+    cartTotal = price + shippingFee
+}
+console.log(cartTotal)
+
+
+document.getElementById("cart2").innerHTML += "Your cart total is " + cartTotal
+
+//switch (suite for create the quize)
+
